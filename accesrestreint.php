dd28165dc5f77060466c3090575d66ded3942d94
Alexandre-Abdelatif <105432335+Alexandre-Abdelatif@users.noreply.github.com>
2023-06-11 22:54:36 +0200

    Update accesrestreint.php

--- accesrestreint.php
+++ accesrestreint.php
@@ -4,25 +4,25 @@
     <title>Accès restreint</title>
     <style>
         body {
-            background-color: skyblue;
+            background-color: skyblue; /* Couleur de fond de la page */
             display: flex;
             align-items: center;
             justify-content: center;
-            height: 100vh;
+            height: 100vh; /* Hauteur de la page */
             margin: 0;
-            font-family: Arial, sans-serif;
-            font-size: 24px;
-            color: white;
-            text-align: center;
+            font-family: Arial, sans-serif; /* Police de caractères */
+            font-size: 24px; /* Taille de police */
+            color: white; /* Couleur du texte */
+            text-align: center; /* Alignement du texte au centre */
         }
     </style>
 </head>
 <body>
-    <h1>Oh oh ! Au vu de votre rôle, vous n'avez pas accès à cette page !!!</h1>
+    <h1>Oh oh ! Au vu de votre rôle, vous n'avez pas accès à cette page !!!</h1> <!-- Titre de la page -->
     <p>
         <p>
             <p>
-            <button onclick="window.history.back();">Cliquez ici pour retourner à la page précédente</button>
+            <button onclick="window.history.back();">Cliquez ici pour retourner à la page précédente</button> <!-- Bouton pour revenir à la page précédente -->
             </p>
         </p>
     </p>
